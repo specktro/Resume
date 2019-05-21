2b0855fe6fc197b1b1b3ff0474b6032bf697fbcc
specktro <specktro@nonull.mx>
2019-05-21 18:34:10 -0500

    chore: Add profile controller callbacks
    New model structures were added to the project and used them inside the profile controller.

--- Resume/Views/ProfileController.swift
+++ Resume/Views/ProfileController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 // MARK: - ProfileController class
 class ProfileController: UITableViewController {
+    // MARK: - Attributes
+    private lazy var profile: Profile? = {
+        let name = "Miguel Angel Gómez Rivero"
+        let nickname = "specktro"
+        let summary = "An enthusiastic software developer"
+        let tools = "xcode, swift, git, coordinators"
+        let sections = [Section(title: "Experience", url: "", summary: nil), Section(title: "Academic", url: "", summary: nil)]
+        return Profile(name: name, nickname: nickname, summary: summary, tools: tools, sections: sections)
+    }()
+    private var refresh: (Profile) -> () = { _ in
+        debugPrint("This is the default refresh callback implementation")
+    }
+    private var select: (Section) -> () = { section in
+        debugPrint("This is the default select callback implementation, the \(section.title) was choosen")
+    }
     
     // MARK: - UIViewController methods
     override func viewDidLoad() {
@@ -24,20 +39,34 @@ class ProfileController: UITableViewController {
     
     // MARK: - Private methods
     private func customize() {
-        self.title = NSLocalizedString("SPCK Resume", comment: "")
+        self.title = self.profile?.nickname
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: CellIdentifier.profile.rawValue)
     }
     
     // MARK: - Table view stuff
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.profile?.sections.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let section = self.profile?.sections[indexPath.row] else {
+            fatalError("There is not profile configured, yo need a profile to work")
+        }
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.profile.rawValue, for: indexPath)
-        cell.textLabel?.text = "cell \(indexPath.row + 1)"
+        cell.textLabel?.text = section.title
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        guard let section = self.profile?.sections[indexPath.row] else {
+            fatalError("There is not profile configured, yo need a profile to work")
+        }
+        
+        self.select(section)
     }
 }
 
