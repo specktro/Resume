2dd83e0437ad9e7d98321de09ea5cd54870a1e40
specktro <specktro@nonull.mx>
2019-05-22 10:11:53 -0500

    refactor: Update networking with project models
    The Profile and Section structures were configured with new Codable type.

--- Resume/Model/QueryAPI.swift
+++ Resume/Model/QueryAPI.swift
@@ -28,8 +28,8 @@ final class QueryAPI {
      - Parameter fail: a closure for error call back.
      - Parameter completion: a closure for success call back.
      */
-    public func getProfile(fail: @escaping ((NSError) -> (Void)), completion: @escaping ((String) -> (Void))) {
-        if let url = URL(string: "https://gist.githubusercontent.com/specktro/e29134eea63332a72fcf5453aec38eb9/raw/5b31b0e73b8d570ff7b2d9cc14a0b12cff199fb3/profile.json") {
+    public func getProfile(fail: @escaping ((NSError) -> (Void)), completion: @escaping ((Profile) -> (Void))) {
+        if let url = URL(string: "https://gist.githubusercontent.com/specktro/e29134eea63332a72fcf5453aec38eb9/raw/c1dc0aaca6add76cca77455155fddbc672cde37c/profile.json") {
             let profileTask = session.dataTask(with: url) { data, response, error in
                 if let error = error {
                     DispatchQueue.main.async {
@@ -38,10 +38,11 @@ final class QueryAPI {
                     }
                 }
                 else if let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
-                    if let jsonString = String(data: data, encoding: .utf8) {
-                        
+                    let jsonDecoder = JSONDecoder()
+                    
+                    if let profile = try? jsonDecoder.decode(Profile.self, from: data) {
                         DispatchQueue.main.async {
-                            completion(jsonString)
+                            completion(profile)
                         }
                     }
                 }
