07d9a81aa2bb2ba85c8313ce91c64e2cf1dda6dd
specktro <specktro@nonull.mx>
2019-05-21 22:59:18 -0500

    feat: Add detail controller for resume’s section
    The information was removed from profile controller and became an attribute from the main coordinator

--- Resume/Coordinator/MainCoordinator.swift
+++ Resume/Coordinator/MainCoordinator.swift
@@ -14,6 +14,15 @@ final class MainCoordinator {
     // MARK: - Attributes
     public weak var window: UIWindow?
     fileprivate weak var profileControler: ProfileController?
+    fileprivate lazy var profile: Profile? = {
+        let name = "Miguel Angel Gómez Rivero"
+        let nickname = "specktro"
+        let summary = "An enthusiastic software developer"
+        let tools = "xcode, swift, git, coordinators"
+        let sections = [Section(title: "Experience", url: "", summary: "An old iOS developer, with more than nine years working with the apple ecosystem, the iOS SDK is his vocation. \n\nHe forgot the number of developed applications by himself and now he loves to teach all he knows to the new iOS generation."),
+                        Section(title: "Academic", url: "", summary: "Specktro studied at ESCOM - IPN his computer science degree; since he was a child he experienced too much love about the maths and the money :v for that reason he became an engineer.")]
+        return Profile(name: name, nickname: nickname, summary: summary, tools: tools, sections: sections)
+    }()
     
     // MARK: - Initializers
     init(window: UIWindow) {
@@ -23,9 +32,10 @@ final class MainCoordinator {
     }
     
     // MARK: - Private methods
-    func initialFlow() {
+    private func initialFlow() {
         let mainBoard = UIStoryboard(name: StoryBoard.main.rawValue, bundle: nil)
         let profileController = mainBoard.instantiateViewController(withIdentifier: ControllerIdentifier.profile.rawValue) as? ProfileController
+        profileController?.profile = self.profile
         profileController?.refresh = self.refresh
         profileController?.select = self.select
         self.window?.rootViewController = UINavigationController(rootViewController: profileController!)
@@ -40,6 +50,9 @@ extension MainCoordinator {
     }
     
     func select(section: Section) {
-        debugPrint("This is the default select callback implementation, the \(section.title) was choosen")
+        let mainBoard = UIStoryboard(name: StoryBoard.main.rawValue, bundle: nil)
+        let detailController = mainBoard.instantiateViewController(withIdentifier: ControllerIdentifier.detail.rawValue) as? DetailController
+        detailController?.section = section
+        self.profileControler?.navigationController?.pushViewController(detailController!, animated: true)
     }
 }
